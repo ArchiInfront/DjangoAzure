f5bf88dc9feebb4ba72df7542154474a1d4c0baf
hirakawa71 <hirakawa71@outlook.jp>
2017-12-04 14:35:23 +0900

    initial commit

---
+++ DjangoAzure/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\秀樹\documents\Visual Studio 2017\Projects\DjangoAzure\DjangoAzure\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
